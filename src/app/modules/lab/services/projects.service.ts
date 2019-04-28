f14e49672b391cb37b912cb9057a4969a59ec375
Krishna Farvil <fkrishna.dev@gmail.com>
2019-04-28 11:57:47 -0700

    Update skills and ceritifcations

--- src/app/modules/lab/services/projects.service.ts
+++ src/app/modules/lab/services/projects.service.ts
@@ -8,23 +8,23 @@ export class ProjectsService {
   private projects = [
     {
       name: 'todo list',
-      url: 'https://codepen.io/ziga-miklic/full/Fagmh',
+      url: 'assets/files/TodoList/index.html',
       color: "#f5f5f5",
       slug: 'todo-list',
       desc: 'converter tool between the most common numeral systems'
     },
     {
       name: 'card validator',
-      url: 'https://codepen.io/tusharbandal/full/mdujc',
+      url: 'https://stackblitz.com/edit/card-provider?embed=1&file=index.js&hideExplorer=1&hideNavigation=1&view=preview',
       color: "#C5CAE9",
       slug: 'card-validator',
       desc: 'converter tool between the most common numeral systems'
     },
     {
-      name: 'card validator',
-      url: 'hangedman',
-      color: "#C5CAE9",
-      slug: 'hangman',
+      name: 'hangman',
+      url: 'https://card-provider.stackblitz.io/',
+      color: "#CDDAE9",
+      slug: 'card-validator',
       desc: 'converter tool between the most common numeral systems'
     }
   ]
--- src/app/modules/main/components/sections/about/about.component.ts
+++ src/app/modules/main/components/sections/about/about.component.ts
@@ -8,19 +8,35 @@ import { Component, OnInit } from '@angular/core';
 export class AboutComponent implements OnInit {
 
   private skills = [
-    { name: 'c language', src: 'assets/img/skills/c.png', size: 100 },
-    { name: 'python', src: 'assets/img/skills/python.png', size: 120 },
-    { name: 'PHP', src: 'assets/img/skills/php.png', size: 100 },
-    { name: 'html 5', src: 'assets/img/skills/html.png', size: 100 },
-    { name: 'CSS', src: 'assets/img/skills/css.png', size: 100 },
-    { name: 'javascript', src: 'assets/img/skills/js.jpg', size: 100 },
-    { name: 'bootstrap', src: 'assets/img/skills/bootstrap.png', size: 100 },
-    { name: 'uikit', src: 'assets/img/skills/uikit.png', size: 100 },
-    { name: 'sass', src: 'assets/img/skills/sass.png', size: 100 },
-    { name: 'ionic', src: 'assets/img/skills/ionic.png', size: 100 },
-    { name: 'angular', src: 'assets/img/skills/angular.png', size: 100 },
-    { name: 'flask', src: 'assets/img/skills/python.png', size: 120 },
-    { name: 'vagrant', src: 'assets/img/skills/python.png', size: 120 },
+    { name: 'c language', src: 'assets/img/skills/c.png' },
+    { name: 'python', src: 'assets/img/skills/python.png' },
+    { name: 'PHP', src: 'assets/img/skills/php.png' },
+    { name: 'HTML5', src: 'assets/img/skills/html.png' },
+    { name: 'CSS3', src: 'assets/img/skills/css.png' },
+    { name: 'javascript', src: 'assets/img/skills/js.png' },
+    { name: 'bootstrap', src: 'assets/img/skills/bootstrap.png' },
+    { name: 'uikit', src: 'assets/img/skills/uikit.png' },
+    { name: 'sass', src: 'assets/img/skills/sass.png' },
+    { name: 'ionic', src: 'assets/img/skills/ionic.png' },
+    { name: 'angular', src: 'assets/img/skills/angular.png' },
+    { name: 'flask', src: 'assets/img/skills/flask.png' },
+    { name: 'vagrant', src: 'assets/img/skills/vagrant.png' },
+    { name: 'mySQL', src: 'assets/img/skills/mysql.png' },
+    { name: 'postgreSQL', src: 'assets/img/skills/psql.png' },
+    { name: 'codeigniter', src: 'assets/img/skills/codeigniter.png' }
+  ];
+
+  private certificates = [
+    { 
+      desc: 'full stack web developer', 
+      logo: 'assets/img/udacity.png',
+      src: 'assets/files/certificates/udacity-fullstack-web-dev-certificate.pdf' 
+    },
+    { 
+      desc: 'First Aid/CPR/AED', 
+      logo: 'assets/img/redcross.png',
+      src: 'assets/files/certificates/american-redcross-certificate.pdf'
+    },
   ];
 
   constructor() { }
